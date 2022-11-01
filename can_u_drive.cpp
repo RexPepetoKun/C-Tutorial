a7bba57c65c4c06c4d57208f9580169ee450cd8c
RexPepetoKun <jxfoong2020@gmail.com>
2022-11-01 15:57:23 +0800

    edit_can_u_drive

--- can_u_drive.cpp
+++ can_u_drive.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
 using namespace std;
 
-void can_you_drive(int age) {
-
-    int const legal_age{16};
-    int n = (legal_age - age);
-    if (age>=legal_age){
-        cout<<"Yes-you can drive!>>";}
-
-    else {
-        cout<<"Sorry,come back in"<<n<<"years";}
-
-    cout<<"Can you legally old enough to drive?"<<endl;
+int main(){
+    int const legal_age = 16;
+    int age;
     cout<<"How old are you?"<<endl;
     cin>>age;
-    cout<<can_you_drive<<endl;
-}
-
-
-
+    int n =(legal_age-age);
+    
+    if (age>=legal_age){
+            cout<<"Congrats, you're old enough to drive";
+        }
+        else{
+            cout<<"Sorry,you need to wait "<<n<<" more years.";
+        }
+    }
 
 
 
