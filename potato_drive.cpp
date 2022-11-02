c567c9dec872c331fe9af5fb4ef687d83be1534a
RexPepetoKun <jxfoong2020@gmail.com>
2022-11-02 13:17:26 +0800

    edit potato_drive_ add boolalpha at input

--- potato_drive.cpp
+++ potato_drive.cpp
@@ -1,41 +1,32 @@
 #include <iostream>
 using namespace std;
 
+int age;
+bool has_car;
+
 void can_you_drive(int age, bool has_car) {
-    //----WRITE YOUR CODE BELOW THIS LINE----
 
-    cout<<"In func: has_car="<<has_car<< " , age="<<age <<endl;
+    if (age >=16){
 
-    if (has_car == true){
-
-        cout<<"Ck pt 1"<<endl;
-        if (age >=16){
+        if (has_car==true){
             cout<<"Yes-you can drive"<<endl;
         }
         else {
-            cout<<"Sorry, come back after"<<16-age<<"years."<<endl;
+            cout<<"Sorry, buy a car first."<<endl;
         }
     }
     else {
-        cout<<"Ck pt 2"<<endl;
-        cout<<"Sorry,you need to buy a car before you can drive"<<endl;
+        cout<<"Sorry, come back after "<<(16-age)<<" years."<<endl;
     }
-
-
-
 }
+
 int main(){
-    int age;
-    bool has_car{true};
 
-    
-    cout<<"You own a car. true(1) or false(0)?"<<endl;
-    cout<<has_car<<endl;
-    //cin>>has_car;
-    cout<<"Please state your age:"<<endl;
+    cout<<"What's your age?"<<endl;
     cin>>age;
-
-    cout<<"In main: has_car="<<has_car<< " , age="<<age <<endl;
+    cout<<"Do you have a car?"<<endl;
+    cin>>boolalpha>>has_car;  
+    //boolaplha needed to allow user to input true/false.otherwise input only accepts 1 or 0.
 
     can_you_drive(age,has_car);
 }
