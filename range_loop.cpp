daed541b7a06fdd65ee80c108c77f4e4ad1951a1
RexPepetoKun <jxfoong2020@gmail.com>
2022-11-04 00:16:16 +0800

    add range_loop

---
+++ range_loop.cpp
@@ -0,0 +1,30 @@
+// Use range based loop to determine through a vector how many elements are
+// divisible by 3
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> list1{1,2,3,4,5,6,7,8,9,10};
+    int count{0};
+    int denominator{0};
+    cout<<"Type in your desired denominator to determine how many element inside vector list" 
+        <<" are divisible by it"<<endl;
+    cin>>denominator;
+    for(auto i:list1){
+        if (i%denominator==0){
+            (count+=1);
+        }
+    }
+
+    cout<<"Total elements divisible by "<<denominator<<" is "<<count<<endl;
+
+
+
+}
+
+
+
+
+
