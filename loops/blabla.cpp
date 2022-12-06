cac07910797c6871bb6511e1ad857832a7267935
RexPepetoKun <jxfoong2020@gmail.com>
2022-12-07 00:48:58 +0800

    update Readme & 7.Pointers.md

--- loops/blabla.cpp
+++ loops/blabla.cpp
@@ -2,24 +2,17 @@
 #include <vector>
 using namespace std;
 
-
-double get_smallest(vector <int> &list){
-   int smallest = list.at(0);
-   
-   for (int i=1;i<list.size();i++){
-        if (smallest > list.at(i)){
-            smallest = list.at(i);
-        }
-        cout<<smallest;
-    }
-}
 int main(){
-    vector <int> list{2,4,6,8,10};
-    cout<<"How many integers are inside the list? "<<list.size()<<endl;
+    string food = "Pizza";  // A food variable of type string
+    string* ptr =&Pizza;    // A pointer variable, with the name ptr, that stores the address of food
 
-    cout<<"The smallest integer in the list is:"<<get_smallest<<endl;
-   
-    cout<<list.at(0);
-   
+    // Output the value of food (Pizza)
+    cout << food << "\n";
+
+    // Output the memory address of food (0x6dfed4)
+    cout << &food << "\n";
+
+    // Output the memory address of food with the pointer (0x6dfed4)
+    cout << ptr << "\n";
+
 }
-
