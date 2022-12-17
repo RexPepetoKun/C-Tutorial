69fc74991dbcbaef549836e693a9447cdc7adaae
RexPepetoKun <jxfoong2020@gmail.com>
2022-12-17 23:39:11 +0800

    add new OOP files

--- C-Tutorial/draft.cpp
+++ C-Tutorial/draft.cpp
@@ -5,17 +5,11 @@ using namespace std;
 
 int main(){
 
-   vector <int> list {1,2,3,4};
-   char option;
-   char P,p,A,a,M,m,S,s,L,l,Q,q;
-   int member,number,smallest,i,j;
 
-   for(i=0;i<list.size();i++){
-      for(j=i+1;j<list.size();j++){
-         if (list[i]<list[j]){
-            int smallest = list[i];
-         }
-      }
-   }
-   cout<<"Smallest integer is "<<smallest<<endl;
+  
+
+
+
+
+
 }
